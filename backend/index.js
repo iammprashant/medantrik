ddc3759bd6e249d4b29ec01c1348dd2efb7ccf37
iamprashant <mrprashantit@gmail.com>
2023-07-04 01:07:13 +0530

    Day 3

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,10 @@ const port = 5000 //port 3000 for react app
 
 
 //app.use(cors())
-//app.use(express.json());// to use req.body
+app.use(express.json());// to use req.body, it is a middleware
 //Available routes
-// app.use('/api/auth', require('./routes/auth'))
-// app.use('/api/notes', require('./routes/notes'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 
 app.listen(port, () => { 
