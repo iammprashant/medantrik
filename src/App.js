b66fb7ae490d7414ca865824f32d7cb6d6acfa13
iamprashant <mrprashantit@gmail.com>
2023-07-07 06:16:18 +0530

    Day 6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './routes/Home';
 
 
 function App() {
   return (
     <div className='App'>
    <Navbar/>
+   <Home/>
     </div>
   );
-}
+} 
 
-export default App;
+export default App; 
