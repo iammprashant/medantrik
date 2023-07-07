d156896d3f9f0e257949779d40313a4fc02b6056
iamprashant <mrprashantit@gmail.com>
2023-07-07 08:43:32 +0530

    Day 7

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './routes/Home';
+import About from './routes/About';
+import Product from './routes/Product';
+import Blog from './routes/Blog';
+import Contact from './routes/Contact';
 
 
 function App() {
   return (
     <div className='App'>
-   <Navbar/>
-   <Home/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/product" element={<Product/>}></Route>
+          <Route path="/blog" element={<Blog/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-} 
+}
 
 export default App; 
